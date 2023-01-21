471f89b79589ec1c962794500a5e5c3879f19a2f
Rafael Peña <rafael31p@gmail.com>
2023-01-20 21:25:40 -0500

    nuestro primera API

---
+++ src/main/java/com/rafael31p/firtsWeb/FirtsWebApplication.java
@@ -0,0 +1,13 @@
+package com.rafael31p.firtsWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirtsWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirtsWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafael31p/firtsWeb/FirtsWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafael31p.firtsWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirtsWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
